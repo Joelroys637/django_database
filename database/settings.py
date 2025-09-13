71364de18365c5c7bd2a1db4b75485e1803dd30d
joker <97875782+Joelroys637@users.noreply.github.com>
2025-09-13 20:16:03 +0530

    Update settings.py

--- database/settings.py
+++ database/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xr8&x%(z=7imea@8@6%prulcp7s6s6=i#a3fo!!()na(-0ad(*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost","django-database-orx4.onrender.com", ]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost","django-database-orx4.onrender.com"]
 
 
 # Application definition
@@ -123,4 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
