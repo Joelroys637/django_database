9e49bbc5aaf16ebbf818d6c54564a9d1106108e3
joker <97875782+Joelroys637@users.noreply.github.com>
2025-09-13 20:05:48 +0530

    Add files via upload

---
+++ database/firebase_config.py
@@ -0,0 +1,9 @@
+import firebase_admin
+from firebase_admin import credentials, db
+
+# Initialize Firebase only once
+if not firebase_admin._apps:
+    cred = credentials.Certificate("firebase-key.json")  # path to your JSON
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': "https://django-eebe7-default-rtdb.firebaseio.com"  # Replace with your DB URL
+    })
