a7facfd8f2c4758e6793494d59e174a2fde88c4a
joker <97875782+Joelroys637@users.noreply.github.com>
2025-09-14 08:06:48 +0530

    Update firebase_config.py

--- database/firebase_config.py
+++ database/firebase_config.py
@@ -1,14 +1,9 @@
-import os, json
-from firebase_admin import credentials, initialize_app
+import firebase_admin
+from firebase_admin import credentials, db
 
-# Load JSON from environment variable
-cred_dict = json.loads(os.environ["FIREBASE_CREDENTIALS"])
-
-# Replace escaped newlines with real newlines
-cred_dict["private_key"] = cred_dict["private_key"].replace("\\n", "\n")
-
-# Initialize Firebase
-cred = credentials.Certificate(cred_dict)
-initialize_app(cred, {
-    "databaseURL": "https://django-eebe7-default-rtdb.firebaseio.com"
-})
+# Initialize Firebase only once
+if not firebase_admin._apps:
+    cred = credentials.Certificate("firebase-key.json")  # path to your JSON
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': "https://django-eebe7-default-rtdb.firebaseio.com"  # Replace with your DB URL
+    })
